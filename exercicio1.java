6ea37945b8fa195d9c3d929d4fa596665232091f
Iago-LC <iagolc9@gmail.com>
2020-12-09 22:19:13 +0100

    Variables declaradas

--- exercicio1.java
+++ exercicio1.java
@@ -3,3 +3,10 @@ package boletín.pkg2;
 import java.util.Scanner;
 
 public class Exercicio7 {
+
+	public static void main(String[] args) {
+        	int n;
+        	int num;
+        	int cont = 0;
+        	Scanner sc = new Scanner(System.in);
+
